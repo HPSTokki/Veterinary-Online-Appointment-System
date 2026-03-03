44dafe73c1c5a08873a44224c3cba6876d6eed05
HPSTokki <sorachisenpai@gmail.com>
2026-03-03 10:41:58 +0800

    Middleware Added

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes import appointment_route
 from src.exception import DomainException
 from src.routes import user_route, client_route, pet_route
 
 app = FastAPI()
+
+ORIGINS = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.exception_handler(DomainException)
 async def handle_exception(req: Request, exc: DomainException):
